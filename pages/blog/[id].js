84ccd6069a0cefe75005aaa2e399ed09fbb0add0
ryo_hisano <ryo080206@gmail.com>
2020-11-06 17:57:47 +0900

    first commit

---
+++ pages/blog/[id].js
@@ -0,0 +1,44 @@
+export default function BlogId({ blog }) {
+  return (
+    <main>
+      <h1>{blog.title}</h1>
+      <p>{blog.publishedAt}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${blog.detail}`,
+        }}
+      />
+    </main>
+  );
+}
+
+// 静的生成のためのパスを指定します
+export const getStaticPaths = async () => {
+  const key = {
+    headers: {'X-API-KEY': process.env.API_KEY},
+  };
+  const data = await fetch('https://test-andus.microcms.io/api/v1/news', key)
+    .then(res => res.json())
+    .catch(() => null);
+  const paths = data.contents.map(content => `/blog/${content.id}`);
+  return {paths, fallback: false};
+};
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async context => {
+  const id = context.params.id;
+  const key = {
+    headers: {'X-API-KEY': process.env.API_KEY},
+  };
+  const data = await fetch(
+    'https://test-andus.microcms.io/api/v1/news/' + id,
+    key,
+  )
+    .then(res => res.json())
+    .catch(() => null);
+  return {
+    props: {
+      blog: data,
+    },
+  };
+};
