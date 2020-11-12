6c5a78c2cd33954fd819821c5006340ef1dee49a
ryo_hisano <ryo080206@gmail.com>
2020-11-12 23:20:22 +0900

    Initialize repository

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,12 +1,15 @@
+import styles from "../../styles/Home.module.scss";
+
 export default function BlogId({ blog }) {
   return (
-    <main>
-      <h1>{blog.title}</h1>
-      <p>{blog.publishedAt}</p>
+    <main className={styles.main}>
+      <h1 className={styles.title}>{blog.title}</h1>
+      <p className={styles.publishedAt}>{blog.publishedAt}</p>
       <div
         dangerouslySetInnerHTML={{
           __html: `${blog.detail}`,
         }}
+        className={styles.post}
       />
     </main>
   );
@@ -15,26 +18,26 @@ export default function BlogId({ blog }) {
 // 静的生成のためのパスを指定します
 export const getStaticPaths = async () => {
   const key = {
-    headers: {'X-API-KEY': process.env.API_KEY},
+    headers: { "X-API-KEY": process.env.API_KEY },
   };
-  const data = await fetch('https://test-andus.microcms.io/api/v1/news', key)
-    .then(res => res.json())
+  const data = await fetch("https://test-andus.microcms.io/api/v1/news", key)
+    .then((res) => res.json())
     .catch(() => null);
-  const paths = data.contents.map(content => `/blog/${content.id}`);
-  return {paths, fallback: false};
+  const paths = data.contents.map((content) => `/blog/${content.id}`);
+  return { paths, fallback: false };
 };
 
 // データをテンプレートに受け渡す部分の処理を記述します
-export const getStaticProps = async context => {
+export const getStaticProps = async (context) => {
   const id = context.params.id;
   const key = {
-    headers: {'X-API-KEY': process.env.API_KEY},
+    headers: { "X-API-KEY": process.env.API_KEY },
   };
   const data = await fetch(
-    'https://test-andus.microcms.io/api/v1/news/' + id,
-    key,
+    "https://test-andus.microcms.io/api/v1/news/" + id,
+    key
   )
-    .then(res => res.json())
+    .then((res) => res.json())
     .catch(() => null);
   return {
     props: {
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Home({ blog }) {
   return (
     <div>
-      {blog.map(blog => (
+      {blog.map((blog) => (
         <ul key={blog.id}>
-          <li >
+          <li>
             <Link href={`blog/${blog.id}`}>
               <a>{blog.title}</a>
             </Link>
@@ -16,18 +16,18 @@ export default function Home({ blog }) {
   );
 }
 
-
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async () => {
   const key = {
-    headers: {'X-API-KEY': process.env.API_KEY},
+    headers: { "X-API-KEY": process.env.API_KEY },
   };
-  const data = await fetch('https://test-andus.microcms.io/api/v1/news', key)
-    .then(res => res.json())
+  const data = await fetch("https://test-andus.microcms.io/api/v1/news", key)
+    .then((res) => res.json())
     .catch(() => null);
   return {
     props: {
       blog: data.contents,
     },
+    revalidate: 3,
   };
 };
